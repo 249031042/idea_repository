495bf1bf3a2beacb92558fdc5f390a48e9a57850
root <root@itcast.cn>
2020-05-05 19:59:15 +0800

    Merge branch 'master' of github.com:249031042/idea_repository
    # Conflicts:
    #	src/HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("HelloWorld");
         System.out.println("我下班了,要提交到git");
         System.out.println("提交完毕");
+        System.out.println("我上班了,又写了新代码");
     }
 }
